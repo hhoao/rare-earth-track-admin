e1aab5c2f51a0f8ea9e6e3516b40adb77db00013
hhoao <haunghao@foxmail.com>
2022-06-30 21:37:26 +0800

    fix(Layout): 优化滚动条

--- src/components/Layout/Sidebar/index.vue
+++ src/components/Layout/Sidebar/index.vue
@@ -1,29 +1,46 @@
 <template>
-  <scroll-bar>
-    <el-menu
-      :show-timeout="200"
-      :default-active="$route.path"
-      :collapse="isCollapse()"
-      background-color="#304156"
-      text-color="#bfcbd9"
-      active-text-color="#409EFF"
-    >
-      <sidebar-item :routes="routes" />
-    </el-menu>
-  </scroll-bar>
+  <div class="scroll-container">
+    <el-scrollbar class="scroll-wrapper">
+      <el-menu
+        :show-timeout="200"
+        :default-active="$route.path"
+        :collapse="isCollapse()"
+        background-color="#304156"
+        text-color="#bfcbd9"
+        active-text-color="#409EFF"
+      >
+        <sidebar-item :routes="routes" />
+      </el-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
 <script setup>
 import SidebarItem from './SidebarItem';
-import ScrollBar from '@/components/ScrollBar';
 import {computed} from 'vue';
 import {useStore} from 'vuex';
 
-const store = useStore()
-const sidebar = computed(() => store.getters.sidebar)
-const routes = computed(() => store.getters.routers)
+const store = useStore();
+const sidebar = computed(() => store.getters.sidebar);
+const routes = computed(() => store.getters.routers);
 
 const isCollapse = () => {
-  return !sidebar.value.opened
-}
+  return !sidebar.value.opened;
+};
 </script>
+<style lang="scss">
+@import '@/styles/variables.scss';
+
+.scroll-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background-color: $menuBg;
+
+  .scroll-wrapper {
+    position: absolute;
+    width: 100% !important;
+  }
+
+}
+</style>
